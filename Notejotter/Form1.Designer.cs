3f40ceb9466c5d0b9cff3c5803ef4d3f6b02c4a3
Dev <Dev@DESKTOP-SB52SRT>
2017-01-16 15:09:37 -0800

    Add project files.

---
+++ Notejotter/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Notejotter
+{
+    partial class NoteJotterMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuRedo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFonts = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.saveFD = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.EnableAutoDragDrop = true;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1211, 547);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1211, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.toolStripMenuItem1,
+            this.mnuQuit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.Size = new System.Drawing.Size(152, 22);
+            this.mnuNew.Text = "New";
+            this.mnuNew.Click += new System.EventHandler(this.mnuNew_Click);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(152, 22);
+            this.mnuOpen.Text = "Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(152, 22);
+            this.mnuSave.Text = "Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuQuit
+            // 
+            this.mnuQuit.Name = "mnuQuit";
+            this.mnuQuit.Size = new System.Drawing.Size(152, 22);
+            this.mnuQuit.Text = "Quit";
+            this.mnuQuit.Click += new System.EventHandler(this.mnuQuit_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuUndo,
+            this.mnuRedo,
+            this.mnuCut,
+            this.mnuCopy,
+            this.mnuPaste});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // mnuCut
+            // 
+            this.mnuCut.Name = "mnuCut";
+            this.mnuCut.Size = new System.Drawing.Size(152, 22);
+            this.mnuCut.Text = "Cut";
+            // 
+            // mnuUndo
+            // 
+            this.mnuUndo.Name = "mnuUndo";
+            this.mnuUndo.Size = new System.Drawing.Size(152, 22);
+            this.mnuUndo.Text = "Undo";
+            // 
+            // mnuRedo
+            // 
+            this.mnuRedo.Name = "mnuRedo";
+            this.mnuRedo.Size = new System.Drawing.Size(152, 22);
+            this.mnuRedo.Text = "Redo";
+            // 
+            // mnuCopy
+            // 
+            this.mnuCopy.Name = "mnuCopy";
+            this.mnuCopy.Size = new System.Drawing.Size(152, 22);
+            this.mnuCopy.Text = "Copy";
+            // 
+            // mnuPaste
+            // 
+            this.mnuPaste.Name = "mnuPaste";
+            this.mnuPaste.Size = new System.Drawing.Size(152, 22);
+            this.mnuPaste.Text = "Paste";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFonts});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // mnuFonts
+            // 
+            this.mnuFonts.Name = "mnuFonts";
+            this.mnuFonts.Size = new System.Drawing.Size(152, 22);
+            this.mnuFonts.Text = "Fonts";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSettings});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // mnuSettings
+            // 
+            this.mnuSettings.Name = "mnuSettings";
+            this.mnuSettings.Size = new System.Drawing.Size(152, 22);
+            this.mnuSettings.Text = "Settings";
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            // 
+            // NoteJotterMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 571);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "NoteJotterMain";
+            this.Text = "NoteJotter";
+            this.Load += new System.EventHandler(this.NoteJotterMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuQuit;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuCut;
+        private System.Windows.Forms.ToolStripMenuItem mnuUndo;
+        private System.Windows.Forms.ToolStripMenuItem mnuRedo;
+        private System.Windows.Forms.ToolStripMenuItem mnuCopy;
+        private System.Windows.Forms.ToolStripMenuItem mnuPaste;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuFonts;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuSettings;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.SaveFileDialog saveFD;
+    }
+}
+
---
+++ Notejotter/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notejotter
+{
+    public partial class NoteJotterMain : Form
+    {
+        string title = " - NoteJotter";
+        bool fileSaved = true;
+
+        public NoteJotterMain()
+        {
+            InitializeComponent();
+            this.Text = "Untitled" + title;
+        }
+
+        // Open file
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            string openFile = "";
+
+            
+            openFD.Title = "Open text file";
+            openFD.Filter = "TXT files|*.txt";
+            openFD.FileName = "";
+            openFD.DefaultExt = ".txt";
+
+            if (openFD.ShowDialog() != DialogResult.Cancel && openFD.CheckFileExists)
+            {
+                openFile = openFD.FileName;
+                richTextBox1.LoadFile(openFile, RichTextBoxStreamType.PlainText);
+                this.Text = openFile + title;
+                fileSaved = true;
+            }
+        }
+
+        // Save file
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void NoteJotterMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Set bool fileChanged to FALSE when something happens
+            fileSaved = false;
+        }
+
+        // New file
+        private void mnuNew_Click(object sender, EventArgs e)
+        {
+            if (fileSaved)
+            {
+                // Clear text
+                NewFile();
+            }
+            else
+            {
+                DialogResult dr = new DialogResult();
+                dr = MessageBox.Show("This file has not been saved yet.\nDo you wish to save it now?",
+                    "Warning!",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+                {
+                    // Save file
+                    Console.WriteLine("Save this file");
+                    SaveFile("thenNew");
+                }
+                else if (dr == DialogResult.No)
+                {
+                    // Don't save file 
+                    Console.WriteLine("Discard changes");
+                    NewFile();
+                }
+                else
+                {
+                    // Stop new file, keep current file
+                    Console.WriteLine("Cancel new file");
+                }
+            }
+        }
+
+        // Empty the whole screen for a new file after confirming save of discarding file
+        void NewFile()
+        {
+            richTextBox1.Clear();
+            this.Text = "Untitled" + title;
+        }
+
+        // Save current file either by clicking the menu option or by warning message
+        void SaveFile()
+        {
+            //because a SaveFile without arguments is okay
+            SaveFile("continue");
+        }
+        void SaveFile(string next)
+        {
+            string saveFile = "";
+
+            saveFD.Title = "Save text file";
+            saveFD.Filter = "TXT files|*.txt";
+            saveFD.FileName = "";
+            saveFD.DefaultExt = ".txt";
+
+            if (saveFD.ShowDialog() != DialogResult.Cancel)
+            {
+                saveFile = saveFD.FileName;
+                richTextBox1.SaveFile(saveFile, RichTextBoxStreamType.PlainText);
+                //MessageBox.Show("File <b>" + saveFile + "</b> saved.",
+                 //   "Saved");
+                this.Text = saveFile + title;
+
+                fileSaved = true;
+            }
+
+            switch (next)
+            {
+                case "thenNew":
+                    // Open new empty file
+                    NewFile();
+                    break;
+                case "thenExit":
+                    Quit();
+                    break;
+                default:
+                    // Continue this file
+
+                    break;
+            }
+        }
+
+        private void mnuQuit_Click(object sender, EventArgs e)
+        {
+            if (fileSaved)
+            {
+                // Clear text
+                Quit();
+            }
+            else
+            {
+                DialogResult dr = new DialogResult();
+                dr = MessageBox.Show("This file has not been saved yet.\nDo you wish to save it now?",
+                    "Warning!",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+                {
+                    // Save file
+                    Console.WriteLine("Save this file");
+                    SaveFile("thenExit");
+                }
+                else if (dr == DialogResult.No)
+                {
+                    // Don't save file 
+                    Console.WriteLine("Discard changes");
+                    Quit();
+                }
+                else
+                {
+                    // Stop new file, keep current file
+                    Console.WriteLine("Cancel exiting");
+                }
+            }
+        }
+
+        void Quit()
+        {
+            //Application.Exit();
+            this.Close();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (fileSaved)
+            {
+                // Just exit
+                //Quit();
+            }
+            else
+            {
+                DialogResult dr = new DialogResult();
+                dr = MessageBox.Show("This file has not been saved yet.\nDo you wish to save it now?",
+                    "Warning!",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+                {
+                    e.Cancel = true;
+                    // Save file
+                    Console.WriteLine("Save this file");
+                    SaveFile("thenExit");
+                }
+                else if (dr == DialogResult.No)
+                {
+                    // Don't save file 
+                    Console.WriteLine("Discard changes");
+                    //Quit();
+                }
+                else
+                {
+                    // Stop new file, keep current file
+                    Console.WriteLine("Cancel exiting");
+                    e.Cancel = true;
+                }
+            }
+
+
+            //e.Cancel = true;
+            base.OnFormClosing(e);
+        }
+
+        /* protected override void OnFormClosing(FormClosingEventArgs e)
+         {
+             if (DialogResult == DialogResult.OK)
+             {
+                 if (!fileSaved)
+                 {
+                     e.Cancel = true;
+                     return;          // Is not calling the base class OnFormClosing okay here?
+                 }
+             }
+             base.OnFormClosing(e);
+         }*/
+    }
+}
